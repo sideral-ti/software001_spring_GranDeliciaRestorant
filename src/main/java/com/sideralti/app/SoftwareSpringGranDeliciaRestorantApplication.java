bf609f9c35effd872864cedde1c3ba08d31d5fde
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:01:43 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringGranDeliciaRestorantApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringGranDeliciaRestorantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringGranDeliciaRestorantApplication.class, args);
+	}
+
+}
